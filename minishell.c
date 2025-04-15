c56832dcf1893a87e9a2f08c27728a9d2d767d97
Dida <diogribe@c1r8s1.42porto.com>
2025-04-15 17:58:44 +0100

    echo e input parser cmd

--- minishell.c
+++ minishell.c
@@ -6,17 +6,54 @@
 /*   By: diogribe <diogribe@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 15:27:21 by diogribe          #+#    #+#             */
-/*   Updated: 2025/04/11 21:22:50 by diogribe         ###   ########.fr       */
+/*   Updated: 2025/04/15 17:46:15 by diogribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	free_cmd(t_cmd *cmd)
+{
+	int	i;
+
+	i = 0;
+	if (!cmd)
+		return;
+	if (cmd->args)
+	{
+		i = 0;
+		while (cmd->args[i])
+		{
+			free(cmd->args[i]);
+			i++;
+		}
+		free(cmd->args);
+	}
+	free(cmd);
+}
+
+t_cmd	*parse_input(char *input)
+{
+	t_cmd	*cmd;
+
+	cmd = malloc(sizeof(t_cmd));
+	if (!cmd)
+		return NULL;
+	cmd->args = ft_split(input, ' ');
+	if (!cmd->args || !cmd->args[0])
+	{
+		free_cmd(cmd);
+		return (NULL);
+	}
+	cmd->cmd = cmd->args[0];
+	return (cmd);
+}
+
 int main(void)
 {
-	char *input;
-	char **args;
-	char *cwd;
+	char	*input;
+	char	*cwd;
+	t_cmd	*cmd;
 
 	while (1)
 	{
@@ -25,38 +62,44 @@ int main(void)
 			break;
 		if (ft_strlen(input) > 0)
 			add_history(input);
-		args = ft_split(input, ' ');
-		if (!args || !args[0])
+		cmd = parse_input(input);
+		if (!cmd)
 		{
 			free(input);
 			continue;
 		}
-		if (ft_strncmp(args[0], "exit", 5) == 0)
-		{
-			free(input);
+		if (ft_strncmp(cmd->cmd, "exit", 5) == 0)
 			break;
-		}
-		else if (ft_strncmp(args[0], "pwd", 4) == 0)
+		else if (ft_strncmp(cmd->cmd, "pwd", 4) == 0)
 		{
-			*cwd = getcwd(NULL, 0);
+			cwd = getcwd(NULL, 0);
 			printf("%s\n", cwd);
 			free(cwd);
 		}
-		else if (ft_strncmp(args[0], "cd", 3) == 0)
+		else if (ft_strncmp(cmd->cmd, "cd", 3) == 0)
 		{
-			if (!args[1])
+			if (!cmd->args[1])
 				chdir(getenv("HOME"));
-			else if (chdir(args[1]) != 0)
+			else if (chdir(cmd->args[1]) != 0)
 				perror("cd");
 		}
+		else if (ft_strncmp(cmd->cmd, "echo", 5) == 0)
+		{
+			if (ft_strlen(input) < 5)
+				printf("\n");
+			else if (ft_strncmp(cmd->args[1], "-n", 3) == 0)
+				printf("%s", input + 7);
+			else
+				printf("%s\n", input + 5);
+		}
 		else
-			printf("zsh: command not found: %s\n", args[0]);
+			printf("zsh: command not found: %s\n", cmd->cmd);
 		// Free dos tokens
-		for (int i = 0; args[i]; i++)
-			free(args[i]);
-		free(args);
+		free_cmd(cmd);
 		free(input);
 	}
+	free_cmd(cmd);
+	free(input);
 	rl_clear_history();
 	return 0;
 }
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: diogribe <diogribe@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 15:57:43 by diogribe          #+#    #+#             */
-/*   Updated: 2025/04/09 16:23:29 by diogribe         ###   ########.fr       */
+/*   Updated: 2025/04/15 16:29:32 by diogribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,12 @@
 #include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+
+typedef struct	s_cmd
+{
+	char *cmd;
+	char **args;
+}				t_cmd;
+
 
 #endif
