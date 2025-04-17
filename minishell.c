b9fc5b78359bd7540f619aa75d85824a9f9995ed
Dida <diogribe@c1r8s1.42porto.com>
2025-04-17 18:05:54 +0100

    ctrl-D, ctrl-C, ctrl-\

--- minishell.c
+++ minishell.c
@@ -6,11 +6,13 @@
 /*   By: diogribe <diogribe@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 15:27:21 by diogribe          #+#    #+#             */
-/*   Updated: 2025/04/24 15:02:20 by diogribe         ###   ########.fr       */
+/*   Updated: 2025/04/30 12:24:40 by diogribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+volatile sig_atomic_t	g_exit_status = 0;
 
 void	handle_sigint(int sig)
 {
@@ -64,12 +66,18 @@ int main(void)
 	char	**cmds;
 	char	*cwd;
 	t_cmd	*cmd;
+	rl_catch_signals = 0;
+	signal(SIGINT, handle_sigint);
+	signal(SIGQUIT, SIG_IGN);
 
 	while (1)
 	{
 		input = readline("$> ");
 		if (!input)
+		{
+			write(1, "exit\n", 5);
 			break;
+		}
 		if (ft_strlen(input) > 0)
 			add_history(input);
 		cmd = parse_input(input);
@@ -86,7 +94,11 @@ int main(void)
 			continue;
 		} */
 		if (ft_strncmp(cmd->cmd, "exit", 5) == 0)
+		{
+			write(1, "exit\n", 5);
+			free_cmd(cmd);
 			break;
+		}
 		else if (ft_strncmp(cmd->cmd, "pwd", 4) == 0)
 		{
 			cwd = getcwd(NULL, 0);
@@ -115,7 +127,6 @@ int main(void)
 		free_cmd(cmd);
 		free(input);
 	}
-	free_cmd(cmd);
 	free(input);
 	rl_clear_history();
 	return 0;
