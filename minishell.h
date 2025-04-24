cdf8e8d7989422dec8c7dca3f82f41c099b1adc1
Dida <nada>
2025-04-24 13:39:01 +0100

    pipes not finished

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: diogribe <diogribe@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 15:57:43 by diogribe          #+#    #+#             */
-/*   Updated: 2025/04/15 16:29:32 by diogribe         ###   ########.fr       */
+/*   Updated: 2025/04/24 13:36:30 by diogribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@
 #include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <signal.h>
+#include <sys/wait.h>
 
 typedef struct	s_cmd
 {
